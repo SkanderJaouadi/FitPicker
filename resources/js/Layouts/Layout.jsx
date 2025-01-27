611fff18fad7d207d7109640d98ebbcfabdc3caf
Skander Jaouadi <iskanderjaouadi99@gmail.com>
2025-01-28 00:42:58 +0100

    pages and routes created

---
+++ resources/js/Layouts/Layout.jsx
@@ -0,0 +1,54 @@
+import { Link } from "@inertiajs/react";
+import Button from "@/components/PrimaryButton";
+import { UserCircle } from "lucide-react";
+
+export default function Layout({ children, isAuth }) {
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      
+      <header className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+          <Link href="/" className="text-3xl font-bold">
+            FIT<span className="text-purple-600">P</span>ICKER
+          </Link>
+          <nav className="flex items-center space-x-4">
+            {isAuth ? (
+              <>
+                <Button variant="ghost" className="hover:text-purple-600" asChild>
+                  <Link href={route('home')}>Home</Link>
+                </Button>
+                <Button variant="ghost" className="hover:text-purple-600" asChild>
+                  <Link href={route('wardrobe')}>My Wardrobe</Link>
+                </Button>
+                <Button variant="ghost" className="hover:text-purple-600" asChild>
+                  <Link href={route('CreateOutfit')}>Create Outfit</Link>
+                </Button>
+                <Button variant="ghost" className="hover:text-purple-600" asChild>
+                  <Link href="/profile">
+                    <UserCircle className="mr-2" />
+                    Account
+                  </Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button variant="ghost" className="hover:text-purple-600" asChild>
+                  <Link href="/login">Sign In</Link>
+                </Button>
+                <Button className="bg-purple-600 hover:bg-purple-700 text-white" asChild>
+                  <Link href="/register">Sign Up</Link>
+                </Button>
+              </>
+            )}
+          </nav>
+        </div>
+      </header>
+
+      
+      <main className="flex-grow bg-gray-100">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</div>
+      </main>
+    </div>
+  );
+}
---
+++ resources/js/Pages/HomePage.jsx
@@ -0,0 +1,23 @@
+
+import Layout from '@/Layouts/Layout';
+import { Head, usePage } from '@inertiajs/react';
+
+export default function HomePage() {
+    const {props}=usePage();
+    const {isAuthicated}=props;
+    return (
+        <Layout isAuth={true}>
+            <Head title="Home" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg ">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            You're logged in!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
